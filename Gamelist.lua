966b5355ea16e068388db30b17eafdf699b84ffb
Moon Hub <3977373683@qq.com>
2025-09-21 12:14:12 +0800

    Create Gamelist.lua

---
+++ Gamelist.lua
@@ -0,0 +1,10 @@
+return {
+    ["地牢英雄"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Dungeon%20Heroes-obfuscated.lua",
+    ["战争大亨"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/WarTycoon-obfuscated.lua",
+    ["排球传奇"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Volleyball%20Legend-obfuscated.lua",
+    ["超速射击"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Hypershot-obfuscated.lua",
+    ["偷饼干"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Steal%20Cookie-obfuscated.lua",
+    ["钓它"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Fish-obfuscated.lua",
+    ["黑暗欺骗[狩猎]"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Hunted-obfuscated.lua",
+    ["死铁轨"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Dead%20rail-obfuscated.lua"
+}

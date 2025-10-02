7fe08c6603731a8c4ec024c2d6b02d078fd289b6
Moon Hub <3977373683@qq.com>
2025-10-02 20:34:22 +0800

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -6,5 +6,14 @@ return {
     ["偷饼干"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Steal%20Cookie-obfuscated.lua",
     ["钓它"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Fish-obfuscated.lua",
     ["黑暗欺骗[狩猎]"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Hunted-obfuscated.lua",
-    ["死铁轨"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Dead%20rail-obfuscated.lua"
+    ["死铁轨"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Dead%20rail-obfuscated.lua",
+    ["砍伐树木"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Cut%20Trees-obfuscated.lua",
+    ["紧急"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Emden-obfuscated.lua",
+    ["战斗勇士"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Combat%20Warriors-obfuscated.lua",
+    ["餐厅大亨3"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Restaurant%20Tycoon%203-obfuscated.lua",
+    ["脑叶公司"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Lobotomy%20Corporation-obfuscated.lua",
+    ["零售大亨2"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Retail%20Tycoon%202-obfuscated.lua",
+    ["启示录"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Revelation-obfuscated.lua",
+    ["萨凡纳生活(草原生活)"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Savannah%20Life-obfuscated.lua",
+    ["森林中99夜"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/99%20Nights%20in%20the%20Forest-obfuscated.lua"
 }

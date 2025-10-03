fc11871eaf800e176d35e38c5a4ed1f6a8f826d9
Moon Hub <3977373683@qq.com>
2025-10-03 19:42:46 +0800

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -9,10 +9,17 @@ return {
     ["死铁轨"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Dead%20rail-obfuscated.lua",
     ["砍伐树木"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Cut%20Trees-obfuscated.lua",
     ["紧急"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Emden-obfuscated.lua",
+    ["战斗勇士"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Combat%20Warriors-obfuscated.lua",
     ["餐厅大亨3"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Restaurant%20Tycoon%203-obfuscated.lua",
     ["脑叶公司"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Lobotomy%20Corporation-obfuscated.lua",
     ["零售大亨2"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Retail%20Tycoon%202-obfuscated.lua",
-    ["启示录"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Revelation-obfuscated.lua",
+    ["启示录"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Apocalypse-obfuscated.lua",
     ["萨凡纳生活(草原生活)"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Savannah%20Life-obfuscated.lua",
-    ["森林中99夜"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/99%20Nights%20in%20the%20Forest-obfuscated.lua"
+    ["森林中99夜"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/99%20Nights%20in%20the%20Forest-obfuscated.lua",
+    ["索纳利亚生活"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Creatures%20of%20Sonaria%20Survive%20Kaiju%20Animals-obfuscated.lua",
+    ["卡车rng"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Truck%20Rng-obfuscated.lua"
+--[[
+    [""] = "",
+    [""] = ""
+]]
 }
